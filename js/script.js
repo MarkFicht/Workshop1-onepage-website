6f6898d20c58b301e214f6353c885b9cf0dbb894
Marek <marekficht@gmail.com>
2018-07-29 17:17:04 +0200

    first script, slider. Still need smoth animation

--- js/script.js
+++ js/script.js
@@ -4,16 +4,24 @@ document.addEventListener('DOMContentLoaded', function () {
     var btnPrev = document.querySelector('.btn-prev');
     var btnNext = document.querySelector('.btn-next');
 
-    var mainOffer = 3;
+    var amountOffer = 3;
+    // amount all offerts from MainOffer slider
+    var currentOffer = 1;
+    
     var changeLeft = document.querySelector('.offer').querySelector('.container').querySelector('.offer__left');
     var changeRight = document.querySelector('.offer').querySelector('.container').querySelector('.offer__right');
 
     btnPrev.addEventListener('click', function () {
         console.log('work.');
+        currentOffer--;
+
+        if (currentOffer === 0) {
+            currentOffer = amountOffer;
+        }
 
         changeLeft.innerHTML = '<img src="images/black_chair.png" alt="black-chair">';
         changeRight.innerHTML = '' +
-            '<h2>1 SIT <span class="color"> ON </span> OUR <span class="color"> CHAIR </span> </h2>\n' +
+            '<h2>'+ currentOffer +' SIT <span class="color"> ON </span> OUR <span class="color"> CHAIR </span> </h2>\n' +
             '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi consectetur esse molestiae nam nobis nulla odio officiis reprehenderit voluptas voluptatum.</p>\n' +
             '<div class="btn"><a href="#">ZOBACZ WIECEJ</a></div>';
 
@@ -21,10 +29,15 @@ document.addEventListener('DOMContentLoaded', function () {
 
     btnNext.addEventListener('click', function () {
         console.log('work too.');
+        currentOffer++;
+
+        if (currentOffer === amountOffer +1) {
+            currentOffer = 1;
+        }
 
         changeLeft.innerHTML = '<img src="images/black_chair.png" alt="black-chair">';
         changeRight.innerHTML = '' +
-            '<h2>2 SIT <span class="color"> ON </span> OUR <span class="color"> CHAIR </span> </h2>\n' +
+            '<h2>'+ currentOffer +' SIT <span class="color"> ON </span> OUR <span class="color"> CHAIR </span> </h2>\n' +
             '<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi consectetur esse molestiae nam nobis nulla odio officiis reprehenderit voluptas voluptatum.</p>\n' +
             '<div class="btn"><a href="#">ZOBACZ WIECEJ</a></div>';
 
